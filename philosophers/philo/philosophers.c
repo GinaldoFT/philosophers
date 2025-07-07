8afdb51dda476711b941bf460c0d03587f20a1c3
ginfranc <gnaldinho79@gmail.com>
2025-07-07 10:51:16 +0100

    add args_check

--- philosophers/philo/philosophers.c
+++ philosophers/philo/philosophers.c
@@ -6,8 +6,52 @@
 /*   By: ginfranc <ginfranc@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 10:18:48 by ginfranc          #+#    #+#             */
-/*   Updated: 2025/07/07 10:19:07 by ginfranc         ###   ########.fr       */
+/*   Updated: 2025/07/07 10:42:18 by ginfranc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	if (!str)
+		return (0);
+	while (str[len])
+		len++;	
+	return (len);
+}
+
+int	args_check(char *av[])
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (av[i])
+	{
+		j = 0;
+		if (ft_strlen(av[i]) == 0)
+			return (1);
+		while (av[i][j])
+		{
+			if (av[i][j] >= '0' && av[i][j] <= '9')
+				j++;
+			else
+				return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	main(int ac, char *av[])
+{
+	if (ac != 6)
+		return (0);
+	if (args_check(av) == 1)
+		return (1);
+	return (0);
+}
